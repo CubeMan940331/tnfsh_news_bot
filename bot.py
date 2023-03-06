7b5a4bc99ea7fa1fbbd897c0a0d99eef052675f8
CubeMan9403331 <CubeMAn940331@gmail.com>
2023-03-06 13:22:46 +0800

    update

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ client=discord.Client(intents=discord.Intents.default())
 async def on_ready():
     while True:
         news_list=spider.get_tnfsh_new()
-        if len(news_list)==0: print('no news detected')
         for item in news_list:
             message=spider.news_to_str(item)
             print(message)
