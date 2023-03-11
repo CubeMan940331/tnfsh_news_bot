e67fa166a6341759242c99fa040737a51f144a45
CubeMan9403331 <CubeMAn940331@gmail.com>
2023-03-11 22:35:41 +0800

    dix bug

--- spider.py
+++ spider.py
@@ -7,13 +7,13 @@ async def tnfsh_news_crawler()->list:
     news is represented as {'title' : `str`, 'date' : `datetime.date`, 'link' : `str`}
     '''
     print(f'[{datetime.datetime.now()}] send request to source')
-    url='https://www.tnfsh.tn.edu.tw/latestevent/Index.aspx?Parser=9,3,19'
-    #url='https://www.tnfsh.tn.edu.tw/latestevent/Index.aspx?Parser=9,3,16'
+    #url='https://www.tnfsh.tn.edu.tw/latestevent/Index.aspx?Parser=9,3,19'
+    url='https://www.tnfsh.tn.edu.tw/latestevent/Index.aspx?Parser=9,3,16'
     response=await httpx.AsyncClient().get(url)
     web_page_text=response.text
     web_page_dom=bs4.BeautifulSoup(web_page_text,'html.parser')
     result=web_page_dom.find_all('ul')[18].find_all('li')
-    p=[]
+    p=deque([])
     for item in result[1:]:
         link=item.find('a').get('href')
         title=item.find('a').get('title')
@@ -21,9 +21,8 @@ async def tnfsh_news_crawler()->list:
         y,m,d=map(int,date_str.split('-'))
         date=datetime.date(year=y,month=m,day=d)
         link=link[link.find('=')+1:]
-        p.append({'title':title,'date':date,'link':link})
-    p.sort(key=lambda arg: arg['date'])
-    return p
+        p.appendleft({'title':title,'date':date,'link':link})
+    return sorted(p,key=lambda arg: arg['date'])
 
 async def get_tnfsh_news()->list:
     '''
